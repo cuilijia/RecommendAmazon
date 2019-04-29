894c3554f1719f97b51ea2ff933ebd70932ff3da
cuilijia <602465981@qq.com>
2019-04-29 14:59:13 +0800

    test input list

--- skipgram.py
+++ skipgram.py
@@ -36,21 +36,15 @@ def addtrain(updatefile):
     model.save('word2vec.model')
     print("Increasing learning complited!")
 
-def test(productid):
+def test(productidlist):
     model = Word2Vec.load('word2vec.model')
-    # for i in range(5):
 
-    print(str(productid)+"-->"+str(model.wv.most_similar([str(productid)])))
-        # , sentences[i][2], sentences[i][3], sentences[i][4]]
+    print(str(productidlist)+"-->"+str(model.wv.most_similar(productidlist)))
 
 # train("data/data1.json")
-
-# for i in range(1,11):
+# for i in range(2,11):
 #     addtrain("data/data"+str(i)+".json")
 
-# addtrain("datageneral/data8.json")
 
-test("18305")
+test(["4831", "13644", "14643", "7826", "12864"])
 
-# 5160-->[('14428', 0.6450660824775696), ('12871', 0.6447880864143372), ('17176', 0.6443230509757996), ('8860', 0.6411486864089966), ('9258', 0.6396166682243347), ('5636', 0.6394439935684204), ('15846', 0.6379180550575256), ('15891', 0.6378426551818848), ('15253', 0.6375370621681213), ('594', 0.6370989084243774)]
-# 5160-->[('9736', 0.9998531341552734), ('4067', 0.9998525381088257), ('496', 0.9998511075973511), ('10099', 0.9998486638069153), ('10226', 0.9998483061790466), ('15266', 0.9998416900634766), ('15408', 0.9998409152030945), ('13649', 0.9998403787612915), ('5925', 0.9998400211334229), ('17950', 0.9998394846916199)]
