cd09eef6ee96ec326ffd76981e1e8189682bfab4
cuilijia <602465981@qq.com>
2019-05-07 03:34:29 +0800

    lstm ok

--- LSTM.py
+++ LSTM.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
 import json
 import numpy as np
-from gensim import corpora
+from gensim.models.word2vec import Word2Vec
+
+def onehot(num):
+    list=[0]*20000
+    list[num]=1
+    return list
 
 def nntrain(x_train, y_train, x_test, y_test):
     # design network
@@ -26,37 +31,60 @@ def getdata(file):
         with open(file,'r') as load_f:
             load_dict = json.load(load_f)
         print(len(load_dict[0]))
+        map(int, load_dict)
         for i in range(len(load_dict[0])):
-            xtrainsent.append(load_dict[0][str(i)][0:8])
-            ytrainsent.append(load_dict[0][str(i)][8:10])
+            # xtrainsent.append(load_dict[0][str(i)][0:8])
+            # ytrainsent.append(load_dict[0][str(i)][8:10])
+            xlist = load_dict[0][str(i)][0:9]
+            Xlist = [int(i) for i in xlist]
+            xtrainsent.append(Xlist)
+            ylist = load_dict[0][str(i)][9:10]
+            Ylist = [int(i) for i in ylist]
+            ytrainsent.append(Ylist)
 
-    dictionary = corpora.Dictionary(xtrainsent)
-    corpus = [dictionary.doc2bow(text) for text in xtrainsent]
-    print(corpus[1])  # [(0, 1), (1, 1), (2, 1)]
-    print(corpus[1])  # [(0, 1), (1, 1), (2, 1)]
+    ytrainsent2=[]
+    for y in ytrainsent:
+        # print(onehot(y[0]))
+        ytrainsent2.append(onehot(y[0]))
+
+    # print("x+y:")
+    # print(xtrainsent)
+    # print(ytrainsent2)
 
     xtrainsent= np.array(xtrainsent)
-    ytrainsent= np.array(ytrainsent)
+    ytrainsent2= np.array(ytrainsent2)
     xtrainsent=xtrainsent.reshape((xtrainsent.shape[0], 1,xtrainsent.shape[1]))
-    return xtrainsent,ytrainsent
+    return xtrainsent,ytrainsent2
 
 def train(xtrainsent,ytrainsent):
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.LSTM(32, input_shape=(xtrainsent.shape[1], xtrainsent.shape[2])))
     model.add(tf.keras.layers.Dense(ytrainsent.shape[1], activation='softmax'))
     # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.fit(xtrainsent, ytrainsent, epochs=10, batch_size=32, verbose=2)
     # model.save('lstm.model')
-    testx=[["8336", "4522", "11338", "6316", "2850", "12419", "8048", "11479"]]
+
+    # next_index = sample(preds=preds, temperatue=temperature)
+    # next_char = char[next_index]
+    return model
+
+def test(model):
+    testx = [["10734", "4320", "7556", "10694", "9153", "1259", "15893", "8780", "4436"]]
     testx = np.array(testx)
     testx = testx.reshape((testx.shape[0], 1, testx.shape[1]))
 
-    testPredict = model.predict(testx)
-    # testPredict = scaler.inverse_transform(testPredict)
-    print(testPredict)
-    return model
-
+    # testPredict = model.predict(testx)
+    preds = model.predict(testx, verbose=0)[0]
+    print(preds)
+    print(max(preds))
+    print(np.argmax(preds))
 
 x,y =getdata("data/data1.json")
+#
+
 model=train(x,y)
+# model.save('lstm.model')
+test(model)
+# model = Word2Vec.load('lstm.model')
+
--- skipgram.py
+++ skipgram.py
@@ -1,7 +1,39 @@
 from gensim.models import Word2Vec
-import json
+
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
+
+import requests
+import json
+
+def getItemsInfo(id):
+
+    itemIndex = int(id)
+    params = {"index": itemIndex}
+    r = requests.get('http://34.80.151.46:10086/getItemInfo', params=params)
+    iteminfo=r.json()
+    print(iteminfo['title'])
+    request_download(str(id), iteminfo['imUrl'])
+    return iteminfo['title']
+
+
+def performance(input, output):
+    a = open("data/Sports_and_Outdoors_Index.json", "r")
+    s = json.load(a)
+    list1 = []
+    for i in input:
+        itemIndex = int(i)
+        params = {"index": itemIndex}
+        r = requests.get('http://34.80.151.46:10086/getAllRelated', params=params)
+        for j in r.json():
+            list1.append(j)
+    list2 = []
+    # print(list1)
+    for k in output:
+        list2.append(s[int(k[0])]["asin"])
+    # print(list2)
+    list3 = list(set(list1).intersection(set(list2)))
+    return len(list3) / len(list2)
 
 
 def train(file,Sg,Size,Window,Min_count,Workers,Iter):
@@ -38,7 +70,8 @@ def addtrain(updatefile):
 def test(productidlist):
     model = Word2Vec.load('word2vec.model')
 
-    print(str(productidlist)+"-->"+str(model.wv.most_similar(productidlist)))
+    # print(str(productidlist)+"-->"+str(model.wv.most_similar(productidlist)))
+    return productidlist,model.wv.most_similar(productidlist)
 
 def drawpic(size):
     model = Word2Vec.load('word2vec.model')
@@ -57,11 +90,57 @@ def drawpic(size):
         pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
     pyplot.show()
 
+def countpre():
+    with open('data/data4.json','r') as load_f:
+        load_dict = json.load(load_f)
+    print(len(load_dict[0]))
+
+    sentences = []
+    for i in range(len(load_dict[0])):
+        # print(load_dict[0][str(i)][:9])
+        # sentences.append(load_dict[0][str(i)][:9])
+        data1=load_dict[0][str(i)][:9]
+        input, output = test(data1)
+        count = performance(input, output)
+        # print(count)
+        print(i)
+        sentences.append(count)
+
+        if (i%100)==0:
+            print(max(sentences))
+
+def request_download(id,url):
+    import requests
+    r = requests.get(url)
+    with open('img/img'+id+'.png', 'wb') as f:
+        f.write(r.content)
+
+# getItemsInfo(11550)
+# drawpic(0)
+# 输入是画的size，为0是全部输出
+
 # train("data/data1.json",Sg=0,Size=250,Window=5,Min_count=1,Workers=4,Iter=10)
 # for i in range(2,11):
 #     addtrain("data/data"+str(i)+".json")
 
-# test(["4831", "13644", "14643"])
+def seachinfolist(input,output):
+    outputx = []
+    for o in output:
+        outputx.append(o[0])
 
-drawpic(0)
-# 输入是画的size，为0是全部输出
+    for it in input:
+        getItemsInfo(it)
+
+    print('output:')
+
+    for it in outputx:
+        getItemsInfo(it)
+
+
+input,output =test(["10929", "10556", "13271", "13359", "17527", "154", "8782", "12977"])
+print(input)
+print(output)
+seachinfolist(input,output)
+
+# count=performance(input, output)
+# print(count)
